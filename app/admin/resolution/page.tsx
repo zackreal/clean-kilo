de2e0cd4ff98a12fd58fbb803bee92bc105bda5f
Giri Purnama <macbookpro2017@macs-MacBook-Pro.local>
2026-08-24 00:25:39 +0700

    Fix 404 on Resolution Center by adding Next.js page route

---
+++ app/admin/resolution/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { AdminResolutionCenter } from "@/components/admin/sections/resolution-center";
+
+export default function ResolutionPage() {
+  return <AdminResolutionCenter />;
+}
